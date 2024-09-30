fc7669322b7faa99c0cca0b3177dc7b9b8897d37
harperl2 <lucas.harper2@mail.dcu.ie>
2024-09-30 20:08:58 +0100

    work

--- DAACOORDINATORAPP/backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from .views import ProductListView
-urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('product/', ProductListView.as_view(), name='product-list'),
-
-]
--- DAACOORDINATORAPP/backend/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Product
-from .serializers import ProductSerializer
-
-class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()[:10]
-    serializer_class = ProductSerializer
