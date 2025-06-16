cd390eef13f2612c6d042aebd39955887628b37c
harperl2 <lucas.harper2@mail.dcu.ie>
2025-06-16 20:15:46 +0100

    New format for operationviews

--- DAACOORDINATORAPP/frontend/src/OperationView.js
+++ DAACOORDINATORAPP/frontend/src/OperationView.js
@@ -1,25 +1,41 @@
-// OperationView.js (Updated with React DnD + Time Left)
 import React from 'react';
 import './OperationView.css';
 import MachineZone from './MachineZone';
 import DraggablePerson from './DraggablePerson';
 
 function OperationView({ onClose, onDuty, assignments, setAssignments, testTime }) {
-    const machineNames = [
-        'ATRS 13', 'ATRS 12', 'ATRS 11', 'ATRS 10', 'ATRS 9',
-        'C3 8', 'C3 7', 'C3 6', 'C3 5', 'C3 4', 'C3 3', 'C3 2', 'C3 1'
-    ];
+    const topMachines = ['C3 1', 'C3 2', 'C3 3', 'C3 4', 'C3 5', 'C3 6'];
+    const bottomMachines = ['C3 7', 'C3 8', 'ATRS 9', 'ATRS 10', 'ATRS 11', 'ATRS 12', 'ATRS 13'];
 
     const moveToMachine = (person, machine) => {
-        setAssignments((prev) => {
+        setAssignments(prev => {
             const updated = { ...prev };
-            Object.keys(updated).forEach((m) => {
+            Object.keys(updated).forEach(m => {
                 updated[m] = updated[m].filter(p => p.id !== person.id);
             });
             updated[machine] = [...(updated[machine] || []), person];
             return updated;
         });
     };
+
+    const renderMachineRow = (machineList) => (
+        <div className="machine-row">
+            {machineList.map(machine => (
+                <div className="machine-wrapper" key={machine}>
+                    <div className="machine-visual">
+                        <div className="machine-top-bar">{machine}</div>
+                        <div className="machine-body" />
+                    </div>
+                    <MachineZone
+                        machine={machine}
+                        assigned={assignments[machine] || []}
+                        moveToMachine={moveToMachine}
+                        testTime={testTime}
+                    />
+                </div>
+            ))}
+        </div>
+    );
 
     return (
         <div className="operation-overlay">
@@ -33,16 +49,9 @@ function OperationView({ onClose, onDuty, assignments, setAssignments, testTime 
                     ))}
                 </div>
 
-                <div className="machines-scroll">
-                    {machineNames.map(machine => (
-                        <MachineZone
-                            key={machine}
-                            machine={machine}
-                            assigned={assignments[machine] || []}
-                            moveToMachine={moveToMachine}
-                            testTime={testTime}
-                        />
-                    ))}
+                <div className="machines-column">
+                    {renderMachineRow(topMachines)}
+                    {renderMachineRow(bottomMachines)}
                 </div>
             </div>
         </div>
